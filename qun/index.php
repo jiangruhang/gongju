64e9b43f5d2a5b7b15f065accf251298f9643a46
jiangruhang <34332149+jiangruhang@users.noreply.github.com>
2019-03-21 21:09:24 +0800

    Add files via upload

---
+++ qun/index.php
@@ -0,0 +1,42 @@
+
+<!doctype html>
+<html lang="zh-CN">
+<head>
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta name="theme-color" content="#fd4b5c">
+<title>QQ加群API - API</title>
+</head>
+<style type="text/css">
+h3:hover {box-shadow:0px 0px 8px #D1D1D1;}
+</style>
+<div style="box-shadow: 5px 5px 25px 0 rgba(46,61,73,.2);border-radius:15px;font-size:13px;width:950px;font-family:微软雅黑,'Helvetica Neue',Arial,sans-serif;margin:10px auto 0px;border:0px solid #eee;max-width:100%;">
+    <div style="width:100%;background-color: #3174ed;background-image: linear-gradient(90deg, #3174ed 0%, #FA8BFF 35%, #3fd9fb 88%);color:#FFFFFF;border-radius:15px 15px 0 0;">
+        <h2 style="font-size:15px;word-break:break-all;padding:20px 32px;margin:0;text-align:center">QQ加群 - API</h2>
+    </div>
+    <div style="margin:0px auto;width:90%">
+        <h3 style="-webkit-transition: all .2s cubic-bezier(0, 0, 0, 0.48);-moz-transition: all .2s ease;border:.0625rem solid #fafafa;background:#fafafa repeating-linear-gradient(-45deg,#fff,#fff 1.125rem,transparent 1.125rem,transparent 2.25rem);margin:15px 0px;padding:20px;border-radius:5px;font-size:14px;color:#333;">
+		# QQ加群API请求方式 #
+		<ul>
+		<li>Method: Post</li>
+		</ul>
+		<hr>
+		# 请求地址 #<br/>
+		<a style="color:#fd4b5c;text-decoration:none;">http://api.ni-co.cn/qun/api.php</a><br/>
+		# 参数 #<br/>
+		<li>guin //QQ群号(必填)</li>
+		<hr>
+		# 返回数据 #<br/>
+		<div style="width:100%;background-color: #000;background-image: linear-gradient(90deg, #6694ed 0%,);color:#FFFFFF;border-radius:5px;word-wrap:break-word;word-break:break-all;">
+	{"code":1,"url":"http://shang.qq.com/wpa/qunwpa?idkey=209c7cf903152da08ae41117e02b9f70f0af03091f4b17c08051a01d38776024"}<br/>	</div>
+		<hr>
+		# 备注 #<br/>
+		-=-=-=-<br/>
+		<hr>
+		# 示例 #<br/>
+		<a style="color:#fd4b5c;text-decoration:none；" target="_blank">http://api.ni-co.cn/qun/api.php?guin=311564207</a>
+		</h3>
+          </div>
+</div>
+</html>
